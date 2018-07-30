d7dfc713f1ecea381c298006e5623df13a7dd778
David Folks <aldwyn.wot@gmail.com>
2018-07-30 13:57:46 -0600

    added detailed search

--- server.js
+++ server.js
@@ -12,8 +12,18 @@ const app = express();
 
 app.use(express.static('public'));
 
+app.get('/api/notes', (req, res) => {
+  res.json(data);
+});
+
+app.get('/api/notes/:id', (req, res) => {
+  
+  res.json(data.find(element => element.id === parseInt(req.params.id)));
+});
+
 app.listen(8080, function () {
   console.info(`Server listening on ${this.address().port}`);
 }).on('error', err => {
   console.error(err);
 });
+
