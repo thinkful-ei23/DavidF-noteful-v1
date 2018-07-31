499eec6d636eb40287f76f8fd9bfbe4ea3de925c
David Folks <aldwyn.wot@gmail.com>
2018-07-31 15:12:55 -0600

    added put method

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
+'use strict';
 
 
 
-
-module.exports.PORT = 8080
+module.exports.PORT = 8080;
--- router/notes.router.js
+++ router/notes.router.js
@@ -2,10 +2,11 @@
 
 const express = require('express');
 const router = express.Router();
-
 const data = require('../db/notes');
 const simDB = require('../db/simDB');  // <<== add this
 const notes = simDB.initialize(data);
+
+router.use(express.json());
 
 
 router.get('/notes', (req, res) => {
@@ -16,10 +17,63 @@ router.get('/notes', (req, res) => {
   }
 });
 
-router.get('/notes/:id', (req, res) => {
-  
-  res.json(data.find(element => element.id === parseInt(req.params.id)));
+router.get('/notes', (req, res, next) => {
+  const { searchTerm } = req.query;
+
+  notes.filter(searchTerm, (err, list) => {
+    if (err) {
+      return next(err); 
+    }
+    res.json(list); 
+  });
 });
+
+
+router.get('/notes/:id', (req, res, next) => {
+  const id = req.params.id;
+  notes.find(id, (err, item) => {
+    if (err) {
+      return next(err);
+    }
+    res.json(item);
+  });
+});
+
+router.put('/notes/:id', (req, res, next) => {
+  const id = req.params.id;
+
+  /***** Never trust users - validate input *****/
+  const updateObj = {};
+  const updateFields = ['title', 'content'];
+
+  updateFields.forEach(field => {
+    if (field in req.body) {
+      updateObj[field] = req.body[field];
+    }
+  });
+
+  if (!updateObj.title) {
+    const err = new Error('Missing `title` in request body');
+    err.status = 400;
+    return next(err);
+  }
+
+  notes.update(id, updateObj, (err, item) => {
+    if (err) {
+      return next(err);
+    }
+    if (item) {
+      res.json(item);
+    } else {
+      next();
+    }
+  });
+});
+
+router.get('/throw', (req, res, next) => {
+  throw new Error('Boom!!');
+});
+
 
 // Post (insert) an item
 router.post('/notes', (req, res, next) => {
