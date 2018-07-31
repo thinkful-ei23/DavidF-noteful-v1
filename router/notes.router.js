6f7e86b0c8b192353edd034325879d6e35ac4e42
David Folks <aldwyn.wot@gmail.com>
2018-07-31 14:16:37 -0600

    added simDB

--- router/notes.router.js
+++ router/notes.router.js
@@ -4,8 +4,8 @@ const express = require('express');
 const router = express.Router();
 
 const data = require('../db/notes');
-
-// const simDB = require('./db/simDB');
+const simDB = require('../db/simDB');  // <<== add this
+const notes = simDB.initialize(data);
 
 
 router.get('/notes', (req, res) => {
@@ -19,6 +19,31 @@ router.get('/notes', (req, res) => {
 router.get('/notes/:id', (req, res) => {
   
   res.json(data.find(element => element.id === parseInt(req.params.id)));
+});
+
+// Post (insert) an item
+router.post('/notes', (req, res, next) => {
+  console.log('Oh hellloooo!');
+  const { title, content } = req.body;
+
+  const newItem = { title, content };
+  /***** Never trust users - validate input *****/
+  if (!newItem.title) {
+    const err = new Error('Missing `title` in request body');
+    err.status = 400;
+    return next(err);
+  }
+
+  notes.create(newItem, (err, item) => {
+    if (err) {
+      return next(err);
+    }
+    if (item) {
+      res.location(`http://${req.headers.host}/notes/${item.id}`).status(201).json(item);
+    } else {
+      next();
+    }
+  });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 // Load array of notes
 
 const express = require('express');
-
+const { PORT } = require('./config');
 const morgan = require('morgan');
 
 const router = require('./router/notes.router');
@@ -15,8 +15,21 @@ app.use(morgan('dev'));
 app.use(express.static('public'));
 app.use('/api', router);
 
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  res.status(404).json({ message: 'Not Found' });
+});
 
-app.listen(8080, function () {
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: err
+  });
+});
+
+app.listen(PORT, function () {
   console.info(`Server listening on ${this.address().port}`);
 }).on('error', err => {
   console.error(err);
